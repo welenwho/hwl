8cb05d8e6aa70fedc74a0e32f082e601b0cb9f06
welenwho <welenwho@gmail.com>
2025-01-26 17:19:20 +0800

    Update GENERAL.txt QCA-ALL.yml README.md

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -56,7 +56,7 @@ UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main" "pkg"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-
+UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main"
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
